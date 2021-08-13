76d1af004b8e84a246651a2c4294445cad2eef4b
DHIRAJ540 <dhirajsubudhi540@gmail.com>
2021-08-13 16:51:29 +0530

    burger change

--- app.js
+++ app.js
@@ -30,3 +30,35 @@ const navSlide = () => {
 }
 
 navSlide();
+
+let x = document.getElementsByTagName("BODY")[0];
+
+x.addEventListener('click' , (e) => {
+    if(!e.target.classList.contains('nav-items') && !e.target.classList.contains('navg') && !e.target.classList.contains('ham') && !e.target.classList.contains('ham-line'))
+    {
+        const burger = document.querySelector(".ham");
+        const nav = document.querySelector(".nav-items");
+        const navItem = document.querySelectorAll(".nav-item");
+        
+        console.log(e.target);
+
+        if(burger.classList.contains('toggle')){
+            console.log('hi');
+            burger.classList.remove('toggle');
+        }
+        if(nav.classList.contains('nav-active')){
+            console.log('hi');
+            nav.classList.remove('nav-active');
+
+            navItem.forEach((link, index) => {
+                if(link.style.animation){
+                    link.style.animation = '';
+                   
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                  
+                }
+            })
+        }
+    }
+})
